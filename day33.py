0817d8413a20070fda501cd7622484f343109fa0
olamide <myyde8170@gmail.com>
2021-12-12 11:38:39 +0100

    added long and lat

---
+++ day33.py
@@ -0,0 +1,20 @@
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+# print(response)
+# print(response.status_code)
+
+response.raise_for_status()
+
+
+data = response.json()
+# data = response.json()['timestamp']
+# print(data)
+
+
+longitude = data['iss_position']['longitude']
+latitude = data['iss_position']['latitude']
+
+iss_position = (longitude, latitude)
+print(iss_position)
