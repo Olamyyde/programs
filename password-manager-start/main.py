8bfd08330b8ccaf50b1b990a85f3eb58f47231f7
olamide <myyde8170@gmail.com>
2022-01-01 11:04:14 +0100

    shortened password generator code further

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,7 +1,7 @@
 from os import write
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import randint, choice, shuffle
 # from PIL import Image, ImageTk
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -10,21 +10,20 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
-
-password_letters = [random.choice(letters) for i in range(nr_letters)]
-password_symbols = [random.choice(letters) for i in range(nr_symbols)]
-password_numbers = [random.choice(letters) for i in range(nr_numbers)]
+password_letters = [choice(letters) for i in range(randint(8, 10))]
+password_symbols = [choice(letters) for i in range(randint(2, 4))]
+password_numbers = [choice(letters) for i in range(randint(2, 4))]
 
 password_list = password_letters + password_symbols + password_numbers
 
-random.shuffle(password_list)
+shuffle(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+
+# password = ""
+# for char in password_list:
+#   password += char
+
+password = "".join(password_list)
 
 print(f"Your password is: {password}")
 
